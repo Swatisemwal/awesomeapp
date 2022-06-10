654b8e960ba4e42d81846bafec5b080ad56bb08c
swatisemwal <swati.semwal17@gmail.com>
2022-06-10 16:29:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to kjhgi;shfhg reload.
+          Edit <code>src/App.js</code> and save to gmghmgkjhgi;shfhg reload.
         </p>
         <a
           className="App-link"
